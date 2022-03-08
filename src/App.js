97eb992a65bccaabae1294c3650721761481ac29
tosin adefope <sirtosin45@gmail.com>
2022-03-08 12:50:35 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Paypal from "./Paypal";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [price, setPrice] = useState('');
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleChange1 = (e) => {
-    setEmail(e.target.value);
-  };
   const handleChange2 = (e) => {
     setPrice(e.target.value);
   };
@@ -20,19 +12,13 @@ function App() {
     <div className="App">
       <div className="name">
         <h1>make a paypal payment</h1>
-        <label htmlFor="name">name</label>
-        <input type="text" onChange={handleChange} />
-        <label htmlFor="email">name</label>
-        <input type="email" onChange={handleChange1} />
-        <input
-          type="text"
-          value={price}
-          onChange={handleChange2}
-        />
+        input amount
+        <br />
+        <input type="text" value={price} onChange={handleChange2} />
       </div>
+      <br />
 
       <Paypal price={price} />
-      {console.log("55", price)}
     </div>
   );
 }
